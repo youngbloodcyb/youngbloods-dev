ebb9feb68b38edb70bac87c243092850d10c3cd2
youngbloodcyb <cameron.youngblood@gmail.com>
2022-10-30 17:13:18 -0600

    change color and update copy

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,21 @@
 </script>
 
 <svelte:head>
-	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,900&display=swap" rel="stylesheet">
 </svelte:head>
 
-<main class="flex flex-col w-screen h-screen font-roboto">
+<main id="main-bg" class="flex flex-col w-screen h-screen font-roboto bg-cream text-black">
 
   <div class="m-6">
     <img src="src/assets/logo-yb.png" alt="yb-logo" width="100">
   </div>
 
-  <div class="flex justify-center items-center w-screen">
-    <div id="main-text-box" class="max-w-max">
-      <h1 class="text-2xl font-bold">Welcome to youngbloods.dev</h1>
+  <div class="flex justify-center items-center w-screen my-24">
+    <div id="main-text-box" class="w-1/3 text-left">
+      <h1 class="lowercase text-6xl italic font-semibold">we are <span class="text-red"><strike>developers</strike></span></h1>
+      <h1 class="lowercase text-6xl italic font-semibold">we are brothers</h1>
     </div>
   </div>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,17 @@ module.exports = {
         'roboto': ['Roboto', 'sans-serif']
       },
     },
+    colors: {
+      'cream' : '#D9C3A9',
+      'purple' : '#9E60A6',
+      'blue' : '#048ABF',
+      'green' : '#02734A',
+      'yellow' : '#F29F05',
+      'red' : '#D93611',
+      'white' : '#FFF',
+      'black' : '#000',
+      'transparent' : 'transparent'
+    }
   },
   plugins: []
 };
